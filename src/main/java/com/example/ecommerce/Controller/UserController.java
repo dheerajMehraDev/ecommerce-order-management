f28112dd3f238ed2730cd72e2076ba8dc3434d05
dm201558 <dm201558@gmail.com>
2026-01-05 01:51:21 +0530

    removed the testing methods from controller layer

--- src/main/java/com/example/ecommerce/Controller/UserController.java
+++ src/main/java/com/example/ecommerce/Controller/UserController.java
@@ -7,31 +7,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user/")
 public class UserController {
 
-    @GetMapping("/getSecretKey")
-    public String getSecretTestMethod(){
-        return "this is secret key ";
-    }
 
-    @GetMapping("/{id}")
-    public UserDto getUerDto(@PathVariable Long id ){
-        return new UserDto(id,"dheeraj", "dheeraj@gmail.com","secretPassword");
-    }
 
-    @GetMapping("/get")
-    public String useRequestParam(@RequestParam Integer age, @RequestParam String sortBy){
-        return " the age is " + age + "and sortBy " + sortBy;
-    }
 
-    @PostMapping
-    public Boolean createUser(@RequestBody UserDto userDto){
-        System.out.println("created the user with user id "+ userDto.getId());
-        return true;
-    }
 
-    @PutMapping
-    public Boolean updateUser(){
-        System.out.println("updated ");
-        return true;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
