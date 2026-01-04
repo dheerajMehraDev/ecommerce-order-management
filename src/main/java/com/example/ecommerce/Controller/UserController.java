5fac48eaaecbf2ee5a714f18d6a92bc51031ee8f
dm201558 <dm201558@gmail.com>
2026-01-05 00:59:24 +0530

    used RestController , GetMapping , Pathvariable , RequestParam; tip : use pathvariable for mandatory fileds

--- src/main/java/com/example/ecommerce/Controller/UserController.java
+++ src/main/java/com/example/ecommerce/Controller/UserController.java
@@ -3,12 +3,13 @@ package com.example.ecommerce.Controller;
 import com.example.ecommerce.DTO.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @GetMapping("/")
+    @GetMapping("/getSecretKey")
     public String getSecretTestMethod(){
         return "this is secret key ";
     }
@@ -16,6 +17,11 @@ public class UserController {
     @GetMapping("/{id}")
     public UserDto getUerDto(@PathVariable Long id ){
         return new UserDto(id,"dheeraj", "dheeraj@gmail.com","secretPassword");
+    }
+
+    @GetMapping("/get")
+    public String useRequestParam(@RequestParam Integer age, @RequestParam String sortBy){
+        return " the age is " + age + "and sortBy " + sortBy;
     }
 
 }
