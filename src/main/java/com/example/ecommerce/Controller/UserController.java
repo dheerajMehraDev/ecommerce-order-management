716f46ffaefc0aca565bcd44a1adbfec89e8cdf9
dm201558 <dm201558@gmail.com>
2026-01-05 13:08:00 +0530

    added controller specific handler

--- src/main/java/com/example/ecommerce/Controller/UserController.java
+++ src/main/java/com/example/ecommerce/Controller/UserController.java
@@ -64,8 +64,11 @@ public class UserController {
     }
 
 
+// handle exception in this controller
 
-
+    public ResponseEntity<String> handlerExceptionInUserController(Exception e){
+        return ResponseEntity.badRequest().build();
+    }
 
 
 
