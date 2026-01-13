196a43d51ab7b0d9cc303afdc267aeb239a2e882
dm201558 <dm201558@gmail.com>
2026-01-13 20:25:10 +0530

    added preauthorize for finer control

--- src/main/java/com/example/ecommerce/Controller/UserController.java
+++ src/main/java/com/example/ecommerce/Controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -43,6 +44,7 @@ public class UserController {
 
 // get all user
 
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping
     public ResponseEntity<List<UserDto>> getAll(){
         return userService.findAllUsers();
